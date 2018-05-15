55f53c7868608478e936fcc8d51371dcb29c91bd
afelua <elena.semenova_2012@mail.ru>
2018-05-15 21:13:23 +0300

    Add javascript file

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var a = 3;
+var b = 5;
+
+var sum = a + b;
+
+console.log(sum);
+
+
+var d = 4;
+var e = 6;
+
+var sum2 = d + e;
+
+console.log(sum2);
+
+
+var text1 = "Пара";
+var text2 = "воз";
+
+var text3 = text1 + text2;
+
+console.log(text3);
+
+var i = 5;
+
+if (i >= 5){
+  console.log('Значение i: ' + i);
+}
+
+
+var myVar = 10;
+
+confirm('Вы - администратор?');
+
